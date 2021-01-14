a772a13d9362e658fae24170b90cb31e809d98e3
Arthur <distorart@gmail.com>
2021-01-14 15:19:46 +0800

    Added Logout Dialog Box

---
+++ LogoutBox.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace TestStudentRegistration
+{
+    partial class LogoutBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Logout = new System.Windows.Forms.Panel();
+            this.label45 = new System.Windows.Forms.Label();
+            this.button_WOC44 = new ePOSOne.btnProduct.Button_WOC();
+            this.button_WOC45 = new ePOSOne.btnProduct.Button_WOC();
+            this.label44 = new System.Windows.Forms.Label();
+            this.Logout.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Logout
+            // 
+            this.Logout.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.Logout.Controls.Add(this.label45);
+            this.Logout.Controls.Add(this.button_WOC44);
+            this.Logout.Controls.Add(this.button_WOC45);
+            this.Logout.Controls.Add(this.label44);
+            this.Logout.Location = new System.Drawing.Point(4, 0);
+            this.Logout.Name = "Logout";
+            this.Logout.Size = new System.Drawing.Size(419, 170);
+            this.Logout.TabIndex = 83;
+            // 
+            // label45
+            // 
+            this.label45.AutoSize = true;
+            this.label45.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label45.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(45)))), ((int)(((byte)(101)))));
+            this.label45.Location = new System.Drawing.Point(160, 63);
+            this.label45.Name = "label45";
+            this.label45.Size = new System.Drawing.Size(87, 30);
+            this.label45.TabIndex = 67;
+            this.label45.Text = "logout?";
+            // 
+            // button_WOC44
+            // 
+            this.button_WOC44.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(45)))), ((int)(((byte)(101)))));
+            this.button_WOC44.ButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(45)))), ((int)(((byte)(101)))));
+            this.button_WOC44.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_WOC44.FlatAppearance.BorderSize = 0;
+            this.button_WOC44.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button_WOC44.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button_WOC44.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_WOC44.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_WOC44.Location = new System.Drawing.Point(222, 98);
+            this.button_WOC44.Name = "button_WOC44";
+            this.button_WOC44.OnHoverBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(153)))), ((int)(((byte)(193)))));
+            this.button_WOC44.OnHoverButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(153)))), ((int)(((byte)(193)))));
+            this.button_WOC44.OnHoverTextColor = System.Drawing.Color.White;
+            this.button_WOC44.Size = new System.Drawing.Size(125, 46);
+            this.button_WOC44.TabIndex = 66;
+            this.button_WOC44.Text = "No";
+            this.button_WOC44.TextColor = System.Drawing.Color.White;
+            this.button_WOC44.UseVisualStyleBackColor = true;
+            this.button_WOC44.Click += new System.EventHandler(this.button_WOC44_Click);
+            // 
+            // button_WOC45
+            // 
+            this.button_WOC45.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(114)))), ((int)(((byte)(168)))));
+            this.button_WOC45.ButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(114)))), ((int)(((byte)(168)))));
+            this.button_WOC45.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_WOC45.FlatAppearance.BorderSize = 0;
+            this.button_WOC45.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button_WOC45.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button_WOC45.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_WOC45.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_WOC45.Location = new System.Drawing.Point(67, 98);
+            this.button_WOC45.Name = "button_WOC45";
+            this.button_WOC45.OnHoverBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(153)))), ((int)(((byte)(193)))));
+            this.button_WOC45.OnHoverButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(153)))), ((int)(((byte)(193)))));
+            this.button_WOC45.OnHoverTextColor = System.Drawing.Color.White;
+            this.button_WOC45.Size = new System.Drawing.Size(121, 46);
+            this.button_WOC45.TabIndex = 65;
+            this.button_WOC45.Text = "Yes";
+            this.button_WOC45.TextColor = System.Drawing.Color.White;
+            this.button_WOC45.UseVisualStyleBackColor = true;
+            this.button_WOC45.Click += new System.EventHandler(this.button_WOC45_Click);
+            // 
+            // label44
+            // 
+            this.label44.AutoSize = true;
+            this.label44.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label44.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(45)))), ((int)(((byte)(101)))));
+            this.label44.Location = new System.Drawing.Point(75, 31);
+            this.label44.Name = "label44";
+            this.label44.Size = new System.Drawing.Size(262, 30);
+            this.label44.TabIndex = 0;
+            this.label44.Text = "Are you sure you want to";
+            // 
+            // LogoutBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 172);
+            this.Controls.Add(this.Logout);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LogoutBox";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "LogoutBox";
+            this.Logout.ResumeLayout(false);
+            this.Logout.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Logout;
+        private System.Windows.Forms.Label label45;
+        private ePOSOne.btnProduct.Button_WOC button_WOC44;
+        private ePOSOne.btnProduct.Button_WOC button_WOC45;
+        private System.Windows.Forms.Label label44;
+    }
+}
---
+++ LogoutBox.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestStudentRegistration
+{
+    public partial class LogoutBox : Form
+    {
+        public LogoutBox()
+        {
+            InitializeComponent();
+        }
+
+        private void button_WOC44_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void button_WOC45_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
